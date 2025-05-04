f6a150c5da570cd8f59a86e49a075ad26ae56d40
rbbozkurt <resitberkaybozkurt@gmail.com>
2025-05-04 16:19:09 +0200

    fix: imports reordered

--- src/main/kotlin/com/rbbozkurt/openbankingapi/controller/HealthController.kt
+++ src/main/kotlin/com/rbbozkurt/openbankingapi/controller/HealthController.kt
@@ -1,7 +1,7 @@
 package com.rbbozkurt.openbankingapi.controller
 
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
